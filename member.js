75f29905f4d8451f4a6c7459cf55216ca9a0137c
Shawn <86738291+OGShawnLee@users.noreply.github.com>
2023-09-23 19:40:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMembers() {
+    return [
+        { name: 'John', skill: 'JavaScript' },
+        { name: 'Jill', skill: 'C#' },
+        { name: 'Jack', skill: 'TypeScript' }
+    ]
+}
